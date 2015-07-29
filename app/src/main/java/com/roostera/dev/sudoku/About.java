4569ba6527ef9fd2d200190d39fd03a1c2e37bf7
Javinchic <javiercruzweb@gmail.com>
2015-07-29 12:43:45 -0500

    about screen added

---
+++ app/src/main/java/com/roostera/dev/sudoku/About.java
@@ -0,0 +1,16 @@
+package com.roostera.dev.sudoku;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Javier on 7/29/2015.
+ */
+public class About extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+}
---
+++ app/src/main/java/com/roostera/dev/sudoku/Sudoku.java
@@ -0,0 +1,70 @@
+package com.roostera.dev.sudoku;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.content.Intent;
+
+
+public class Sudoku extends Activity implements OnClickListener
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Set up click listeners for all the buttons
+        View continueButton = findViewById(R.id.continue_button);
+        continueButton.setOnClickListener(this);
+        View newButton = findViewById(R.id.new_button);
+        newButton.setOnClickListener(this);
+        View aboutButton = findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(this);
+        View exitButton = findViewById(R.id.exit_button);
+        exitButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId()){
+            case R.id.about_button:
+                Intent i = new Intent(this, About.class);
+                startActivity(i);
+                break;
+            //More buttons go here! (if any) ...
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings)
+        {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
